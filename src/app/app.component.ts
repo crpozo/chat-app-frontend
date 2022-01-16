f7bc3708b071164c5bd9fd682f41be714464db40
cpozo <carlosricardo.pozo@hotmail.com>
2022-01-16 22:14:44 +0100

    Add pusher, ngmodel and styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import Pusher from 'pusher-js';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'chat-app-frontend';
+export class AppComponent implements OnInit {
+
+  username:string = 'username';
+  message:any = '';
+  messages:any = [];
+
+  constructor( private http:HttpClient ) {
+
+  }
+  
+  ngOnInit() {
+     // Enable pusher logging - don't include this in production
+     Pusher.logToConsole = true;
+
+     const pusher = new Pusher('b1a4432e58903dd6bed4', {
+       cluster: 'eu'
+     });
+ 
+     const channel = pusher.subscribe('chat');
+     channel.bind('message', data => {
+       this.messages.push(data);
+     });
+  }
+  
+  submit():void {
+    this.http.post('http://localhost:8000/api/messages', {
+      username: this.username,
+      message: this.message,
+    }).subscribe(() => {
+      this.message = '';
+    });
+  }
+
 }
